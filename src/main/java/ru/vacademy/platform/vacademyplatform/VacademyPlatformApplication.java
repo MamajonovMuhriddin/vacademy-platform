b8c42ce4fd2423cb62fa1985945a0f921ef78a5f
MMamajonov <mamajonov_muhriddin@mail.ru>
2023-06-05 22:20:48 +0600

    Initial commit

---
+++ src/main/java/ru/vacademy/platform/vacademyplatform/VacademyPlatformApplication.java
@@ -0,0 +1,13 @@
+package ru.vacademy.platform.vacademyplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacademyPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacademyPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vacademy/platform/vacademyplatform/VacademyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vacademy.platform.vacademyplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacademyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
